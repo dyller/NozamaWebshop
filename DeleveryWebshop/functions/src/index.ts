00a089a71f669cfdfe519ee31ab3cc51a95c0135
Sub-Z3r0 <andersen19@gmail.com>
2019-05-14 11:06:27 +0200

    refactor
    cod

--- DeleveryWebshop/functions/src/index.ts
+++ DeleveryWebshop/functions/src/index.ts
@@ -9,26 +9,26 @@ admin.initializeApp()
 exports.deleteProduct = functions.firestore
   .document('products/{productID}')
   .onDelete((snap, context) => {
-    return new Promise((resolve, reject) => {
-
-
-    // Get an object representing the document prior to deletion
-    // e.g. {'name': 'Marie', 'age': 66}
+    return new Promise(async(resolve, reject) => {
     const deletedProduct = snap.data();
     if(deletedProduct) {
-      admin.firestore().collection('files')
-        .doc(deletedProduct.pictureId)
-        .delete()
-        .then(value => {
-          admin.storage()
-            .bucket()
-            .file('product-pictures/' + deletedProduct.pictureId)
-            .delete()
-            .then(res => resolve(res), err => reject(err))
-            .catch(err => reject(err))
-        }, err => reject(err))
-        .catch(err => reject(err));
-      // perform desired operations ...
+      try{
+        await admin.firestore().collection('files')
+          .doc(deletedProduct.pictureId)
+          .delete()
+          .then();
+
+        const resultFromStorage = await admin.storage()
+          .bucket()
+          .file('product-pictures/' + deletedProduct.pictureId)
+          .delete()
+          .then()
+
+        resolve(resultFromStorage);
+      }
+      catch(err) {
+        reject(err)
+      }
     }
     else {
       reject('No product deleted');
@@ -69,7 +69,7 @@ exports.products = functions.https.onRequest((request, response) => {
       const listOfProducts: any = [];
 
       prods.forEach(product => {
-        let prod = product.data();
+        const prod = product.data();
         prod.id = product.id;
         listOfProducts.push(prod);
       })
