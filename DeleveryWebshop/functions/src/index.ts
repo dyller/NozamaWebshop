5f8fcaaa8a148d9b4c5e5c1cc7b485ca7e0cd1bd
Sub-Z3r0 <andersen19@gmail.com>
2019-05-14 10:20:10 +0200

    delete from db, storage etc functiosn
    FUNCTIONS WORKS NAO

--- DeleveryWebshop/functions/src/index.ts
+++ DeleveryWebshop/functions/src/index.ts
@@ -6,23 +6,59 @@ admin.initializeApp()
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 
+exports.deleteProduct = functions.firestore
+  .document('products/{productID}')
+  .onDelete((snap, context) => {
+    return new Promise((resolve, reject) => {
+
+
+    // Get an object representing the document prior to deletion
+    // e.g. {'name': 'Marie', 'age': 66}
+    const deletedProduct = snap.data();
+    if(deletedProduct) {
+      admin.firestore().collection('files')
+        .doc(deletedProduct.pictureId)
+        .delete()
+        .then(value => {
+          admin.storage()
+            .bucket()
+            .file('product-pictures/' + deletedProduct.pictureId)
+            .delete()
+            .then(res => resolve(res), err => reject(err))
+            .catch(err => reject(err))
+        }, err => reject(err))
+        .catch(err => reject(err));
+      // perform desired operations ...
+    }
+    else {
+      reject('No product deleted');
+    }
+    });
+  });
+
 exports.uploadNewProductImage =
   functions.storage.object().onFinalize((object) => {
 
     // save meta data in firestore
       // Event back DONE
     return new Promise((resolve, reject) => {
-      const filemeta = {
-        name: 'd7UTs2odB9pTYifjvXox',
-        type: 'image/png',
-        size: 402513
-      }
-      if (object && object.metadata) {
-        resolve('Happy days');
+      if (object && object.name && object.metadata) {
+        const fileMeta = {
+          lastModified: object.updated,
+          name: object.metadata.originalName,
+          type: 'image/png',
+          size: object.size
+        }
+        const nameForDoc = object.name.split('/')[1];
+        admin.firestore().collection('files')
+          .doc(nameForDoc)
+          .set(fileMeta)
+          .then(value => resolve(value))
+          .catch(err => reject(err))
         // Firestore and save metadata
       }
       else {
-        reject('Error happened');
+        reject('Error happened, not enough metadata or file data');
       }
     });
   });
