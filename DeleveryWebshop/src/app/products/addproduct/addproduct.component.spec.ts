cd9a2ca90c9a8b3930cafd8b76abf6b2a3f3791b
liverpoljer <liverpoljer@live.dk>
2019-05-20 20:54:28 +0200

    good beginning

--- DeleveryWebshop/src/app/products/addproduct/addproduct.component.spec.ts
+++ DeleveryWebshop/src/app/products/addproduct/addproduct.component.spec.ts
@@ -6,14 +6,19 @@ import {ReactiveFormsModule} from '@angular/forms';
 import {ImageCropperModule} from 'ngx-image-cropper';
 import {RouterTestingModule} from '@angular/router/testing';
 import {ProductService} from '../../shared/service/product.service';
-import {Product} from "../../shared/entities/product";
-import {ImageMetadata} from "../../shared/entities/image-metadata";
-import {Observable} from "rxjs";
+import {Product} from '../../shared/entities/product';
+import {ImageMetadata} from '../../shared/entities/image-metadata';
+import {Observable, of} from 'rxjs';
 
 describe('AddproductComponent', () => {
   let component: AddproductComponent;
   let fixture: ComponentFixture<AddproductComponent>;
+  let psMock: any;
+  let psMockSub: any;
   beforeEach(async(() => {
+    psMock = jasmine.createSpyObj('ProductService', ['addProductWithImage']);
+   psMockSub = jasmine.createSpyObj('addProductWithImage', ['subscribe']);
+    psMock.addProductWithImage.and.returnValue(psMockSub);
     TestBed.configureTestingModule({
       declarations: [ AddproductComponent ],
       imports: [ ReactiveFormsModule,
@@ -21,7 +26,7 @@ describe('AddproductComponent', () => {
         RouterTestingModule
       ],
       providers: [
-        {provide: ProductService, useClass: ProductServiceStub}
+        {provide: ProductService, useValue: psMock}
       ]
     })
       .compileComponents();
@@ -35,6 +40,16 @@ describe('AddproductComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('AddProduct Add', () => {
+    beforeEach(() => {
+      component.addProduct();
+    });
+    it('should call ps.deleteProduct 1 time', () => {
+      expect(psMockSub.subscribe).toHaveBeenCalledTimes(1);
+    });
+  });
+
 
 });
 
