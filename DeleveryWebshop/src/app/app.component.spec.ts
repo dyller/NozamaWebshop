bf50eb67eab769b0371eb86e781c275771c67b03
liverpoljer <liverpoljer@live.dk>
2019-05-21 11:21:13 +0200

    ad

--- DeleveryWebshop/src/app/app.component.spec.ts
+++ DeleveryWebshop/src/app/app.component.spec.ts
@@ -2,8 +2,7 @@ import {TestBed, async, ComponentFixture} from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import {NvbarComponent} from './shared/nvbar/nvbar.component';
-import {ShowproductComponent} from "./products/showproduct/showproduct.component";
-import {DOMHelper} from "../testing/DOMHelper";
+
 
 describe('AppComponent', () => {
   let component: AppComponent ;
@@ -11,7 +10,7 @@ describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule,
+        RouterTestingModule
               ],
       declarations: [
         AppComponent,
@@ -22,9 +21,13 @@ describe('AppComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(AppComponent);
     component = fixture.componentInstance;
+    fixture.detectChanges();
 
   });
-  /*describe('title', () => {
+  /*it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+  describe('title', () => {
 
     it('should be DeleveryWebshop', () => {
       expect(component.title).toBe('DeleveryWebshop');
--- DeleveryWebshop/src/app/order/order/order.component.spec.ts
+++ DeleveryWebshop/src/app/order/order/order.component.spec.ts
@@ -1,10 +1,88 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { OrderComponent } from './order.component';
+import {Observable, of} from 'rxjs';
+import {RouterTestingModule} from '@angular/router/testing';
+import {MatButtonModule, MatCardModule, MatGridListModule, MatTooltipModule} from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {CartService} from '../../shared/service/cart.service';
+import {HttpClientModule} from '@angular/common/http';
+import {DOMHelper} from '../../../testing/DOMHelper';
+import {ReactiveFormsModule} from '@angular/forms';
+import {AngularFireStorageModule} from '@angular/fire/storage';
+import {AngularFireAuthModule} from '@angular/fire/auth';
+import {AngularFireModule} from '@angular/fire';
+import {environment} from '../../../environments/environment';
+import {AngularFirestoreModule} from '@angular/fire/firestore';
+import {Product} from '../../shared/entities/product';
 
 describe('OrderComponent', () => {
   let component: OrderComponent;
   let fixture: ComponentFixture<OrderComponent>;
+  let dh: DOMHelper<OrderComponent>;
+  let productCart: any;
+  beforeEach(async(() => {
+    productCart = jasmine.createSpyObj('CartService', ['getAllProduts', 'addToFB']);
+    productCart.getAllProduts.and.returnValue(of([]));
+    TestBed.configureTestingModule({
+      declarations: [OrderComponent],
+      imports: [
+        ReactiveFormsModule,
+        RouterTestingModule,
+        AngularFireStorageModule,
+        AngularFireAuthModule,
+        MatTooltipModule,
+        BrowserAnimationsModule,
+        MatButtonModule,
+        MatCardModule,
+        MatGridListModule,
+        AngularFireModule.initializeApp(environment.firebase),
+        AngularFirestoreModule, // imports firebase/firestore, only needed for database features
+      ],
+      providers: [
+        {provide: CartService, useValue: productCart}
+      ]
+    })
+      .compileComponents();
+  }));
+  beforeEach(() => {
+    fixture = TestBed.createComponent(OrderComponent);
+    component = fixture.componentInstance;
+    dh = new DOMHelper(fixture);
+    fixture.detectChanges();
+  });
+  describe('Simple HTML', () => {
+    beforeEach(() => {
+      fixture.detectChanges();
+    });
+    // Simple HTML
+    it('should create', () => {
+      expect(component).toBeTruthy();
+    });
 
+    it('should contain an div tag with Order list', () => {
+      expect(dh.singleText('div')).toBe('Order list');
+    });
+
+    it('Should minimum be one button on the page', () => {
+      expect(dh.count('button')).toBeGreaterThanOrEqual(1);
+    });
+
+    it('Should be a Buy button first on the page', () => {
+      expect(dh.singleText('button')).toBe('Buy');
+    });
+  });
 
 });
+class Helper {
+  products: Product[] = [];
+  getProducts(amount: number): Observable<Product[]> {
+    for (let i = 0; i < amount; i++) {
+      this.products.push(
+        {id: 'product1', amount: 1, pictureId: 'picture'
+          , name: 'product', url: 'image', price: 300}
+      );
+    }
+    return of(this.products);
+  }
+}
